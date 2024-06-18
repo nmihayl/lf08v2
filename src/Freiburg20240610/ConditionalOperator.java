877bb56f2e7662eb2ae5e352d2fe8f20b796565f
nmihayl <nikola_mihaylov@gmx.de>
2024-06-18 13:10:29 +0200

    ConditionalOperator, SwitchCase

---
+++ src/Freiburg20240610/ConditionalOperator.java
@@ -0,0 +1,10 @@
+package Freiburg20240610;
+
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 5, max;
+        max = (a > b) ? a : b;
+        System.out.println("Der größte Wert ist: " + max);
+
+    }
+}
---
+++ src/Freiburg20240610/SwitchCase.java
@@ -0,0 +1,21 @@
+package Freiburg20240610;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        int auswahl = 1;
+        switch (auswahl) {
+            case 1:
+                System.out.println("Auswahl 1");
+                break;
+            case 2:
+                System.out.println("Auswahl 2");
+                break;
+            case 3:
+                System.out.println("Auswahl 3");
+                break;
+            default:
+                System.out.println("kein Auswahl");
+                break;
+        }
+    }
+}
