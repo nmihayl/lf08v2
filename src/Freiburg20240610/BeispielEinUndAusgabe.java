825c7da1899af0077021d498bcef8f8bf226833b
nmihayl <nikola_mihaylov@gmx.de>
2024-06-18 10:41:55 +0200

    BeispielEinUndAusgabe

--- src/Freiburg20240610/BeispielEinUndAusgabe.java
+++ src/Freiburg20240610/BeispielEinUndAusgabe.java
@@ -10,8 +10,14 @@ public class BeispielEinUndAusgabe {
         String name = input.nextLine();
         System.out.println("Geben Sie bitte ihr Alter ein: ");
         int alter = input.nextInt();
+        System.out.println("Geben Sie bitte ihre Körpergröße ein: ");
+        double groesse = input.nextDouble();
+        System.out.println("Geben Sie bitte ihr Gewicht ein: ");
+        double gewicht = input.nextDouble();
 
-        System.out.println("Hallo " + name + "! Du bist " + alter + " Jahre alt.");
+        // Ausgabe der Daten
+        System.out.println("Sie heissen " + name + ", sind " + alter + " Jahre alt und sind " + groesse + " m groß und wiegen " + gewicht + " Kilogramm.");
+        input.close();
     }
 
 }
